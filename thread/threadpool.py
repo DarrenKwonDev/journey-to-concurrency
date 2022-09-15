9205ecee4426d8f791bb291212a5f71cdf295a36
DarrenKwonDev <anatomy1545@gmail.com>
2022-09-15 21:30:49 +0900

    study: threadpoolexecutor

--- thread/threadpool.py
+++ thread/threadpool.py
@@ -12,12 +12,12 @@ if __name__ == '__main__':
     format = "%(asctime)s: [%(threadName)s] %(message)s"
     logging.basicConfig(level=logging.DEBUG, format=format, datefmt="%H:%M:%S")
 
-    def main():
-        pass
+    print(f"main pid: {os.getpid()}")
 
     def task(name):
+        logging.debug(f"sub thread native id: {threading.get_native_id()}")
         time.sleep(1)
-        return f'{name} done'
+        return f'{name} {threading.get_native_id()} done'
 
     with ThreadPoolExecutor(max_workers=3) as executor:
         tasks = executor.map(task, ["task1", "task2", "task3"])
